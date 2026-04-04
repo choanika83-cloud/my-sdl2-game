140a85ae137d2d368160d6f6b2edd08a4bdf08f7
Anika Subha Kabir Chowdhury <273137494+choanika83-cloud@users.noreply.github.com>
2026-04-04 18:35:38 +0600

    circle

--- src/app.cpp
+++ src/app.cpp
@@ -24,3 +24,32 @@ using namespace std;
     }
 }
 
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Circle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) isRunning = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw the outline in white
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        drawCircle(renderer, 400, 300, 100);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
+
