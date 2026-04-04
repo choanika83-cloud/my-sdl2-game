10510b52dc693d993da96d092625c2986256d568
Anika Subha Kabir Chowdhury <273137494+choanika83-cloud@users.noreply.github.com>
2026-04-04 18:22:35 +0600

    just circle

--- src/app.cpp
+++ src/app.cpp
@@ -1,44 +1,26 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <iostream>
-
-int main(int argc, char* argv[]) {
-    // 1. Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    // 2. Create the Window
-    // In SDL2, we use SDL_WINDOWPOS_UNDEFINED and a 'flags' argument (like SDL_WINDOW_SHOWN)
-    SDL_Window* window = SDL_CreateWindow(
-        "Hello SDL2 Window", 
-        SDL_WINDOWPOS_UNDEFINED, 
-        SDL_WINDOWPOS_UNDEFINED, 
-        800, 300, 
-        SDL_WINDOW_SHOWN
-    );
-
-    if (window == nullptr) {
-        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    // 3. Main Loop
-    bool isRunning = true;
-    SDL_Event event;
-    while (isRunning) {
-        while (SDL_PollEvent(&event)) {
-            // In SDL2, it is SDL_QUIT (not SDL_EVENT_QUIT)
-            if (event.type == SDL_QUIT) {
-                isRunning = false;
+using namespace std;
+// function for drawing using general equation of a circle
+ void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+    int thickness = 1; 
+    
+    for (int i = 0; i < radius * 2 + thickness; i++) {
+        for (int j = 0; j < radius * 2 + thickness; j++) {
+            int dx = radius - i; 
+            int dy = radius - j;
+            
+            // Calculation of distance squared
+            int Distsq = (dx * dx + dy * dy);
+            
+            // Check if the pixel is on the "edge"
+            // We check if it's between (r-1)^2 and (r)^2
+            if (Distsq >= (radius - thickness) * (radius - thickness) && 
+                Distsq <= (radius * radius)) {
+                SDL_RenderDrawPoint(renderer, centerX + dx, centerY + dy);
             }
         }
     }
-
-    // 4. Cleanup
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
 }
+
