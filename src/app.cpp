cd409b888bf32a76ea94e4af6667c820af20960a
choanika83-cloud <273137494+choanika83-cloud@users.noreply.github.com>
2026-04-05 12:39:24 +0600

    Update app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -7,19 +7,19 @@ using namespace std;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-// function for drawing circle using general equation of a circle
+// Function for drawing circle using general equation of a circle
  void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
     int thickness = 1; 
     
-    for (int i = 0; i < radius * 2 + thickness; i++) {
-        for (int j = 0; j < radius * 2 + thickness; j++) {
-            int dx = radius - i; 
-            int dy = radius - j;
+    for (int w = 0; w < radius * 2 + thickness; w++) {
+        for (int h = 0; h < radius * 2 + thickness; h++) {
+            int dx = radius - w; 
+            int dy = radius - h;
             
             // Calculation of distance squared
             int Distsq = (dx * dx + dy * dy);
             
-            // Check if the pixel is on the "edge"
+            // Check if the pixel is on the edge
             // We check if it's between (r-1)^2 and (r)^2
             if (Distsq >= (radius - thickness) * (radius - thickness) && 
                 Distsq <= (radius * radius)) {
@@ -61,11 +61,11 @@ int main(int argc, char* argv[]) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set color to Black
     SDL_RenderClear(renderer);                      // Clear the whole screen
 
-    // 4. THE DRAWING
+    // The drawing
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Set color to White
     drawCircle(renderer, 400, 300, (int)currentRadius);   // Draw the NEW circle
 
-    // 5. THE DISPLAY (Must happen AFTER drawing)
+    // Display circle
     SDL_RenderPresent(renderer);
 
         // Frame rate limit so the growth is visible
