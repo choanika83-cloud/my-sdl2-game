d1ebf9c878e6c70b00d85e4364cd23ca2d47fe83
choanika83-cloud <273137494+choanika83-cloud@users.noreply.github.com>
2026-04-05 13:48:18 +0600

    Add files via upload

---
+++ app.cpp
@@ -0,0 +1,55 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+using namespace std;
+// function for drawing using general equation of a circle
+ void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+    int thickness = 1;
+
+    for (int w = 0; w < radius * 2 + thickness; w++) {
+        for (int h = 0; h < radius * 2 + thickness; h++) {
+            int dx = radius - w;
+            int dy = radius - h;
+
+            // Calculation of distance squared
+            int Distsq = (dx * dx + dy * dy);
+
+            // Check if the pixel is on the edge
+            // We check if it's between (r-1)^2 and (r)^2
+            if (Distsq >= (radius - thickness) * (radius - thickness) &&
+                Distsq <= (radius * radius)) {
+                SDL_RenderDrawPoint(renderer, centerX + dx, centerY + dy);
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Circle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) isRunning = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw the outline in white
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        drawCircle(renderer, 400, 300, 100);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
+
